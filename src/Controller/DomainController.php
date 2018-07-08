896dbe324131bed37003f2b00a519bbdb9279201
VolCh <volch5@gmail.com>
2018-07-08 16:51:14 +0300

    Update the most of outdated composer deps & fix Symfony 4.1 deprecations

--- src/Controller/DomainController.php
+++ src/Controller/DomainController.php
@@ -1,10 +1,10 @@
 <?php
 namespace App\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 /**
  * Controller used to test domains.
